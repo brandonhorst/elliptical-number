59d1ac82a34604cf0b8d73740a386476cac2a011
= <brandonhorst@gmail.com>
2014-10-30 09:54:33 -0400

    Added float, updated lacona version

--- test/test.js
+++ test/test.js
@@ -1,4 +1,3 @@
-var async = require('async');
 var chai = require('chai');
 var expect = chai.expect;
 var number = require('../lib/number')
@@ -11,17 +10,22 @@ describe('number', function () {
 	var parser;
 
 	beforeEach(function () {
-		parser = new Parser().understand(number);
+		parser = new Parser({sentences: ['test']});
 	});
 
 	it('handles a valid integer', function (done) {
-		var schema = {
-			root: {
-				type: 'integer',
-				id: 'test'
-			},
-			run: ''
-		}
+		var grammar = {
+			phrases: [
+				{
+					name: 'test',
+					root: {
+						type: 'integer',
+						id: 'test'
+					},
+				}
+			],
+			dependencies: [number]
+		};
 
 		var handleData = sinon.spy(function (data) {
 			expect(data.match[0].string).to.equal('1234');
@@ -30,52 +34,100 @@ describe('number', function () {
 
 		var handleEnd = function () {
 			expect(handleData).to.have.been.called.once;
-			done()
+			done();
 		};
 
 		parser
-		.understand(schema)
+		.understand(grammar)
 		.on('data', handleData)
 		.on('end', handleEnd)
 		.parse('1234')
 	});
+
+	it('rejects an invalid integer', function (done) {
+		var grammar = {
+			phrases: [
+				{
+					name: 'test',
+					root: {
+						type: 'integer',
+						id: 'test'
+					},
+				}
+			],
+			dependencies: [number]
+		};
+
+		var handleData = sinon.spy();
+
+		var handleEnd = function () {
+			expect(handleData).to.not.have.been.called;
+			done();
+		};
+
+		parser
+		.understand(grammar)
+		.on('data', handleData)
+		.on('end', handleEnd)
+		.parse('123.4')
+	});
+
+	it('handles a valid float', function (done) {
+		var grammar = {
+			phrases: [
+				{
+					name: 'test',
+					root: {
+						type: 'float',
+						id: 'test'
+					},
+				}
+			],
+			dependencies: [number]
+		};
+
+		var handleData = sinon.spy(function (data) {
+			expect(data.match[0].string).to.equal('12.34');
+			expect(data.result.test).to.equal(12.34);
+		});
+
+		var handleEnd = function () {
+			expect(handleData).to.have.been.called.once;
+			done();
+		};
+
+		parser
+		.understand(grammar)
+		.on('data', handleData)
+		.on('end', handleEnd)
+		.parse('12.34')
+	});
+
+	it('rejects an invalid float', function (done) {
+		var grammar = {
+			phrases: [
+				{
+					name: 'test',
+					root: {
+						type: 'float',
+						id: 'test'
+					},
+				}
+			],
+			dependencies: [number]
+		};
+
+		var handleData = sinon.spy();
+
+		var handleEnd = function () {
+			expect(handleData).to.not.have.been.called;
+			done();
+		};
+
+		parser
+		.understand(grammar)
+		.on('data', handleData)
+		.on('end', handleEnd)
+		.parse('123.4.34')
+	});
 });
-		// var testCases = [
-		// 	input: '1234'
-		// 	desc: 'valid'
-		// 	schema:
-		// 	match: '1234'
-		// 	result:
-		// 		test: 1234
-		// 	matches: 1
-		// ,
-		// 	input: '12b4'
-		// 	desc: 'invalid'
-		// 	schema:
-		// 		root:
-		// 			type: 'integer'
-		// 		run: ''
-		// 	matches: 0
-		// ,
-		// 	input: '12'
-		// 	desc: 'more than max'
-		// 	matches: 0
-		// ,
-		// 	input: '12'
-		// 	desc: 'less than min'
-		// 	matches: 0
-		// ,
-		// 	input: '12'
-		// 	desc: 'single number (max and min are inclusive)'
-		// 	schema:
-		// 		root:
-		// 			type: 'integer'
-		// 			max: 12
-		// 			min: 12
-		// 			id: 'test'
-		// 		run: ''
-		// 	match: '12'
-		// 	result: 
-		// 		test: 12
-		// 	matches: 1
-		// ]

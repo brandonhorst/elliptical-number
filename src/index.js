d3cf36cd2fec3b0f5770b0f7a525eaaca10b5dab
Jeff Malins <jmalins@iolani.org>
2016-02-19 08:36:22 -1000

    added simple Decimal type

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export {default as Integer} from './integer'
 export {default as DigitString} from './digitstring'
 export {default as Ordinal} from './ordinal'
+export {default as Decimal} from './decimal'
